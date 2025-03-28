7d2c51fff7f729e90995accece8e5cbbd4688254
Tharunthorn Musik <tharunthornmusik1446@gmail.com>
2025-03-28 20:51:19 +0700

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Set the script to exit on any error
+set -e
+
+# Pull the latest changes from the repository
+echo "Pulling latest changes from Git..."
+git fetch origin
+git reset --hard origin/main
+
+echo "Bringing down Docker containers..."
+docker-compose down
+
+echo "Starting Docker containers in detached mode..."
+docker-compose up -d
+
+echo "Deployment complete!"
+
+# Make the script executable with:
+# chmod +x deploy.sh
+
+# Then run it with:
+# ./deploy.sh
